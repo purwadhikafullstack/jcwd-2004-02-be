929518dd63d7784a196fc03f4a2d18a924b06ffa
Ampuh <ampuh.rakan@gmail.com>
2022-07-14 20:57:21 +0700

    fpg2-45

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -1,19 +1,13 @@
 const productControllers = require("./productControllers");
 const authControllers = require("./authControllers");
 const profileControllers = require("./profileControllers");
-const transactionControllers = require('./transactionControllers')
-
+const transactionControllers = require("./transactionControllers");
 
 const transactionControllers = require("./transactionControllers");
 
 module.exports = {
   productControllers,
   authControllers,
-<<<<<<< HEAD
   profileControllers,
   transactionControllers,
-=======
-  profileControllers, 
-  transactionControllers
->>>>>>> 26b27b6dab7be49d6c210f8809437a445504f7f8
 };
--- src/routes/index.js
+++ src/routes/index.js
@@ -2,12 +2,7 @@ const productRoutes = require("./productRoutes");
 const authRoutes = require("./authRoutes");
 const profileRoutes = require("./profileRoutes");
 const prescriptionRoutes = require("./prescriptionRoutes");
-<<<<<<< HEAD
 const transactionRoutes = require("./transactionRoutes");
-=======
-const transactionRoutes = require('./transactionRoutes')
-
->>>>>>> 26b27b6dab7be49d6c210f8809437a445504f7f8
 
 module.exports = {
   productRoutes,
@@ -15,5 +10,5 @@ module.exports = {
   profileRoutes,
   transactionRoutes,
   prescriptionRoutes,
-  transactionRoutes
+  transactionRoutes,
 };
